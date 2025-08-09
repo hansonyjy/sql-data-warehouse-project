a6bdde73792ee427618a960895338bed437c309c
Hanson Yang <55715529+hansonyjy@users.noreply.github.com>
2025-08-08 22:18:11 -0700

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -21,12 +21,13 @@ CREATE TABLE silver.crm_cust_info(
 
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
+    cat_id VARCHAR(50),
 	prd_key VARCHAR(50),
 	prd_nm VARCHAR(50),
 	prd_cost INT,
 	prd_line VARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
+	prd_start_dt DATE,
+	prd_end_dt DATE,
     dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
